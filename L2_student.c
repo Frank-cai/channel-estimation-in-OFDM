1a3c0f7041c28e49e4a57f13dd992cf7df9469c9
Frank-cai <40657099+Frank-cai@users.noreply.github.com>
2022-05-14 21:40:29 +0200

    Add files via upload

--- L2_student.c
+++ L2_student.c
@@ -9,11 +9,14 @@
 
 void generate_pilot_qpsk(double pltI[], double pltQ[], int len) {
     // Randomly generate QPSK symbols
-
+	double tmp1, tmp2;
 	//***Student_code_start*** 
-	//    Your code...
-	//    ...
-	//    ...
+	for(int i = 0; i < len; i++){
+		tmp1 = rand_ra();
+		tmp2 = rand_ra();
+		pltI[i] = (tmp1 > 0.5) ? SCALE_QPSK : -SCALE_QPSK;
+		pltQ[i] = (tmp2 > 0.5) ? SCALE_QPSK : -SCALE_QPSK;
+	}
 	//***Student_code_end*****
 }
 
@@ -29,9 +32,10 @@ void generate_pilot_qpsk(double pltI[], double pltQ[], int len) {
 
 void insert_pilot_tdm(double frmI[], double frmQ[], double pltI[], double pltQ[], int len) {
 	//***Student_code_start*** 
-	//    Your code...
-	//    ...
-	//    ...
+	for(int i = 0; i < len; i++){
+		frmI[i] = pltI[i];
+		frmQ[i] = pltQ[i];
+	}
 	//***Student_code_end*****
 }
 
@@ -56,10 +60,21 @@ void insert_pilot_tdm(double frmI[], double frmQ[], double pltI[], double pltQ[]
 
 void get_data_pilot_indexes(int dataIndx[], int pltIndx[], int numCarriers, int numPilots, int lastPilot){
 	//***Student_code_start*** 
-	//    Your code...
-	//    ...
-	//    ...
-	//***Student_code_end*****lse dataIndx[j++] = i;
+	int m = 0, n = 0;
+	if(lastPilot == 1){
+		for(int i = 0; i < numCarriers; i++){
+			if(i == (floor(m*(numCarriers-1.0)/(numPilots-1)))){pltIndx[m] = i;m++;}
+			else{dataIndx[n] = i;n++;}
+		}
+	}
+	else{
+		for(int i = 0; i < numCarriers; i++){
+			if(i % (numCarriers/numPilots) == 0){pltIndx[m] = i;m++;}
+			else{dataIndx[n] = i;n++;}
+		}
+	}
+
+	//***Student_code_end***
 
 }
 
@@ -81,9 +96,10 @@ void get_data_pilot_indexes(int dataIndx[], int pltIndx[], int numCarriers, int 
 
 void insert_symbols_fdm(double frmI[], double frmQ[], double symI[], double symQ[], int symIndx[], int symLen) {
 	//***Student_code_start*** 
-	//    Your code...
-	//    ...
-	//    ...
+	for(int i = 0; i < symLen; i++){
+		frmI[symIndx[i]] = symI[i];
+		frmQ[symIndx[i]] = symQ[i];
+	}
 	//***Student_code_end*****
 }
 
@@ -111,9 +127,10 @@ void insert_symbols_fdm(double frmI[], double frmQ[], double symI[], double symQ
 
 void ch_estimation(double Hi[], double Hq[], double rxSigI[], double rxSigQ[], double pltI[], double pltQ[], int len) {
 	//***Student_code_start*** 
-	//    Your code...
-	//    ...
-	//    ...
+	for(int i = 0; i < len; i++){
+		Hi[i] = (rxSigI[i]*pltI[i]+rxSigQ[i]*pltQ[i])/(pltI[i]*pltI[i]+pltQ[i]*pltQ[i]);
+		Hq[i] = (rxSigQ[i]*pltI[i]-rxSigI[i]*pltQ[i])/(pltI[i]*pltI[i]+pltQ[i]*pltQ[i]);
+	}
 	//***Student_code_end*****
 }
 
@@ -131,9 +148,28 @@ void ch_estimation(double Hi[], double Hq[], double rxSigI[], double rxSigQ[], d
 
 void filter_noise(double Hi[], double Hq[], int len){
 	//***Student_code_start*** 
-	//    Your code...
-	//    ...
-	//    ...
+	double* hi = (double*) malloc((SYMBOLS_PER_BLOCK) * sizeof(double));
+    double* hq = (double*) malloc((SYMBOLS_PER_BLOCK) * sizeof(double));
+	for(int i = 0; i < len; i++){
+		hi[i] = 0;
+		hq[i] = 0;
+		for(int j = 0; j < len; j++){
+			hi[i] += (Hi[j]*cos(2 * PI * i * j / len)-Hq[j]*sin(2 * PI * i * j / len));
+			hq[i] += (Hi[j]*sin(2 * PI * i * j / len)+Hq[j]*cos(2 * PI * i * j / len));
+		}
+	}
+
+	for(int i = 0; i < len; i++){
+		Hi[i] = 0;
+		Hq[i] = 0;
+		for(int j = 0; j < MAX_TAPS; j++){
+			Hi[i] += (hi[j]*cos(- 2 * PI * i * j / len)-hq[j]*sin(- 2 * PI * i * j / len));
+			Hq[i] += (hi[j]*sin(- 2 * PI * i * j / len)+hq[j]*cos(- 2 * PI * i * j / len));
+		}
+	}
+	free(hi);
+	free(hq);
+
 	//***Student_code_end*****
 }
 
@@ -163,9 +199,10 @@ void filter_noise(double Hi[], double Hq[], int len){
 
 void ch_estimation_fdm(double Hi[], double Hq[], double rxSigI[], double rxSigQ[], double pltI[], double pltQ[], int pltIndx[], int pltLen) {
 	//***Student_code_start*** 
-	//    Your code...
-	//    ...
-	//    ...
+	for(int i = 0; i < pltLen; i++){
+		Hi[pltIndx[i]] = (rxSigI[pltIndx[i]]*pltI[i]+rxSigQ[pltIndx[i]]*pltQ[i])/(pltI[i]*pltI[i]+pltQ[i]*pltQ[i]);
+		Hq[pltIndx[i]] = (rxSigQ[pltIndx[i]]*pltI[i]-rxSigI[pltIndx[i]]*pltQ[i])/(pltI[i]*pltI[i]+pltQ[i]*pltQ[i]);
+	}
 	//***Student_code_end*****
 }
 
@@ -182,9 +219,14 @@ void ch_estimation_fdm(double Hi[], double Hq[], double rxSigI[], double rxSigQ[
 
 void interp_linear(double Hi[], double Hq[], int pltIndx[], int numCarriers, int numPilots) {
 	//***Student_code_start*** 
-	//    Your code...
-	//    ...
-	//    ...
+	int n = 0;
+	for(int i = 0; i < numCarriers; i++){
+		if(pltIndx[n] == i){n++;continue;}
+		else{
+			Hi[i] = (Hi[pltIndx[n]]-Hi[pltIndx[n-1]])/(pltIndx[n]-pltIndx[n-1])*(i-pltIndx[n-1])+Hi[pltIndx[n-1]];
+			Hq[i] = (Hq[pltIndx[n]]-Hq[pltIndx[n-1]])/(pltIndx[n]-pltIndx[n-1])*(i-pltIndx[n-1])+Hq[pltIndx[n-1]];
+		}
+	}
 	//***Student_code_end*****
 }
 
@@ -202,9 +244,27 @@ void interp_linear(double Hi[], double Hq[], int pltIndx[], int numCarriers, int
 
 void interp_quadratic(double Hi[], double Hq[], int pltIndx[], int numCarriers, int numPilots) {
   	//***Student_code_start*** 
-	//    Your code...
-	//    ...
-	//    ...
+	int n = 0;
+	double c0 = 0, c1 = 0, c2 = 0;
+	int k;
+	k = numCarriers / numPilots;
+	for(int i = 0; i < numCarriers; i++){
+		if(i == pltIndx[n]){n++;continue;}
+		else if (n == numPilots){
+			c0 = 1.0*(pltIndx[n]-i-2*k)*(pltIndx[n]-i-k)/(2*k*k);
+			c1 = 1.0*(pltIndx[n]-i)*(2*k-pltIndx[n]+i)/(k*k);
+			c2 = 1.0*(pltIndx[n]-i)*(pltIndx[n]-i-k)/(2*k*k);
+			Hi[i] = c0*Hi[pltIndx[n]]+c1*Hi[pltIndx[n-1]]+c2*Hi[pltIndx[n-2]];
+			Hq[i] = c0*Hq[pltIndx[n]]+c1*Hq[pltIndx[n-1]]+c2*Hq[pltIndx[n-2]];
+		}
+		else{
+			c0 = 1.0*(i-pltIndx[n-1]-2*k)*(i-pltIndx[n-1]-k)/(2*k*k);
+			c1 = 1.0*(i-pltIndx[n-1])*(2*k-i+pltIndx[n-1])/(k*k);
+			c2 = 1.0*(i-pltIndx[n-1])*(i-pltIndx[n-1]-k)/(2*k*k);
+			Hi[i] = c0*Hi[pltIndx[n-1]]+c1*Hi[pltIndx[n]]+c2*Hi[pltIndx[n+1]];
+			Hq[i] = c0*Hq[pltIndx[n-1]]+c1*Hq[pltIndx[n]]+c2*Hq[pltIndx[n+1]];
+		}
+	}
 	//***Student_code_end*****
 }
 
@@ -223,9 +283,24 @@ void interp_quadratic(double Hi[], double Hq[], int pltIndx[], int numCarriers, 
 
 void interp_fft(double Hi[], double Hq[], int pltIndx[], int numCarriers, int numPilots) {
   	//***Student_code_start*** 
-	//    Your code...
-	//    ...
-	//    ...
+	int n = 0;
+	int k;
+	k = numCarriers / numPilots;
+	double phiI,phiQ;
+	for(int i = 0; i < numCarriers; i++){
+		if(i == pltIndx[n]){n++;continue;}
+		else{
+			Hi[i] = 0;
+			Hq[i] = 0;
+			for(int q = 0; q < numPilots; q++){
+				phiI = 1.0/numPilots*sin(PI*(i-k*q)/k)/sin(PI*(i-k*q)/numCarriers)*cos(-PI*(i-k*q)*(numCarriers-k)/numCarriers/k);
+				phiQ = 1.0/numPilots*sin(PI*(i-k*q)/k)/sin(PI*(i-k*q)/numCarriers)*sin(-PI*(i-k*q)*(numCarriers-k)/numCarriers/k);
+				Hi[i] += phiI*Hi[pltIndx[q]]-phiQ*Hq[pltIndx[q]];
+				Hq[i] += phiQ*Hi[pltIndx[q]]+phiI*Hq[pltIndx[q]];
+			}
+		}	
+	}
+	filter_noise(Hi, Hq, numCarriers);
 	//***Student_code_end*****
 }
 
